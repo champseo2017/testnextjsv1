444746b0b9160210fc1cdfd5bacbe5b51594b16a
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-25 02:55:01 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -20,6 +20,7 @@ class Layout extends Component {
                 <title>Learn Nexs.js</title>
                 <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+                <link rel="shortcut icon" href="https://www.img.in.th/images/56eb79bdec4b3f2d58faa52defcc2b00.png"></link>
                 <style>{`
                 html,body{
                     height: 100%;
